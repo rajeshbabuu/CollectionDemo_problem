a7574c9619ca35f9c9619112649a14dccf4f8501
Rajesh kumar <rajeshbabu656656@gmail.com>
2023-02-01 22:14:25 +0530

    Dictionary

---
+++ CollectionDemoProblem/DemoClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionDemoProblem
+{
+    public class DemoClass
+    {
+        public void DictionaryDemo()
+        {
+            Console.WriteLine("\n**********Dictionary Demo**********");
+
+            Dictionary<int, string> objDictionary = new Dictionary<int, string>();
+            objDictionary.Add(1, "Person1");
+            objDictionary.Add(2, "Person2");
+            objDictionary.Add(3, "Person3");
+
+            Console.WriteLine("\nAccess value using key(key=1): " + objDictionary[1]);
+
+            Console.WriteLine("\nIterating Dictionary: ");
+            foreach (var element in objDictionary)
+            {
+                Console.WriteLine($"Key = {element.Key} & Value = {element.Value}");
+            }
+        }
+    }
+}
---
+++ CollectionDemoProblem/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace CollectionDemoProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Collection Demo Classes");
+            DemoClass objDemo = new DemoClass();
+            objDemo.DictionaryDemo();
+        }
+    }
+}
